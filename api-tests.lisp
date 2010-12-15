289c90f6d67e625e38189cd2dbf9d27c34bee6de
Greg Pfeil <greg@technomadic.org>
2010-12-14 22:50:11 -0500

    Added some more tests.

--- api-tests.lisp
+++ api-tests.lisp
@@ -11,3 +11,29 @@
 
 (test should-reduce-across-types
   (is (equal '("X") (parse (==> 'x #'symbol-name) '(x)))))
+
+(test should-match-based-on-predicate
+  (is (equal '(#\x) (parse (token #'characterp) '(#\x))))
+  (is (equal '() (parse (token #'characterp) '(12)))))
+
+(test should-survive-right-recursion
+  (lazy-let ((right-recurse (choice (~ 'x right-recurse) *epsilon*)))
+    (is (equal '((x x x x x x x)) (parse right-recurse '(x x x x x x x))))))
+
+(test should-survive-left-recursion
+  (lazy-let ((left-recurse (choice (~ left-recurse 'x) *epsilon*)))
+    (is (equal '((((((((NIL . X) . X) . X) . X) . X) . X) . X))
+               (parse left-recurse '(x x x x x x x))))))
+
+(test should-normalize-left-recursion
+  (lazy-let ((left-recurse (choice (==> (~ left-recurse 'x)
+                                        (lambda (parse)
+                                          (append (car parse)
+                                                  (list (cdr parse)))))
+                                   *epsilon*)))
+    (is (equal '((x x x x x x x)) (parse left-recurse '(x x x x x x x))))))
+
+(test should-survive-bidi-recursion
+  (lazy-let ((bidi (choice (~ bidi '+ bidi) (==> 'x #'list))))
+    (is (equal '(((X) + (X) + (X) + (X) + (X) + (X) + (X) + X) + X)
+               (car (parse bidi '(x + x + x + x + x + x + x + x + x)))))))
